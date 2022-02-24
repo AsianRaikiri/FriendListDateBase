d792e8e050763bbb5d7d53ca76bab644178ea172
Maik Grunicke <h.m.grunicke@gmail.com>
2022-02-24 20:47:36 +0100

    Initializing first SQL Database Code
    This is the rough structure for my Database

---
+++ FriendListDB.sql
@@ -0,0 +1,9 @@
+CREATE TABLE FriendList (
+        friendID INT PRIMARY KEY,
+        name VARCHAR(20),
+        lastName VARCHAR(20)
+);
+
+Describe friendlist;
+
+ALTER TABLE friendlist ADD favoriteColor VARCHAR(10);
